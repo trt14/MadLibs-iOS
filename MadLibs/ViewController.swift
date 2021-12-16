a698f13e182223e7ba17b4f2f1eb40888d56be9e
trt14 <tarooti14dev@gmail.com>
2021-12-16 10:31:07 +0300

    Initial Commit

---
+++ MadLibs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MadLibs
+//
+//  Created by Tarooti on 12/05/1443 AH.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MadLibsTests/MadLibsTests.swift
@@ -0,0 +1,33 @@
+//
+//  MadLibsTests.swift
+//  MadLibsTests
+//
+//  Created by Tarooti on 12/05/1443 AH.
+//
+
+import XCTest
+@testable import MadLibs
+
+class MadLibsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
